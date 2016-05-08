7414fd71fc6e947324235a49ea67af282a320228
feelShuang <2216336012@qq.com>
2016-05-08 11:25:33 +0800

    alipay

--- Common.h
+++ Common.h
@@ -35,13 +35,13 @@
 /** 支付宝相关信息 */
 
 // 商户PID
-#define AlipayPartner @"2088911913468208"
+#define AlipayPartner @""
 // 商户收款账号
-#define AlipaySeller @"service@lansongys.com"
+#define AlipaySeller @""
 // 商户私钥
-#define AlipayRSA_PrivateKey @"MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBANuQiFs6eyEINMyFn/kvINgjYSFisdxxBcBQdbmA6fkxPqn+ytM+JHFkIFsNbco6fuW7vMWhyieADDjwoXrkAamvVgC4LvucjkqL2EWmPpfXce6qpGeQqRCKc0z3ZduHDvMJPc32S9yIrK90N4MhWhm9rVbNFNaAUqgNPMEXkeXjAgMBAAECgYA6jHwP97RFxq5tq2RRSmPzvttzz6GmKu1hrxL1eI7ryFnxcuQbGIMXSZ/nJ9mTB643DKz19oK1rcuUk3Y8EvjuC/cD6hpwMJ6OFNvgw5Bya9of8W63Ve+OMT2RmKZ2HshfS/PnlsLQ03+M2krchzAKqweDt3eroy043Dog/w0gAQJBAP4+Sb+TLXc6Y3ZXzb090R+oJbUEk87uCGrYEfurRonbmYaY3Q40gWprROh6zjG5YXrjH4wwR7njbho3ImRZGeMCQQDdFOdpmmyKewEbzSUfxXMKVw9HjyzPoj6L1YySufSY6Jxrr9irABmTUnZL34d+xCRwsjXctHbxqiwKiICfrcQBAkAC5dzbVScgg8bcc3XB4XF/xd/gJ1Qz+JyZ8yqJTtN4AMvIL/fdEJYlC2H2sGenQ3CsAOi8JVS79q6rl9NJh4Z3AkEAja+E83/9Se608i1SOn9fT+QlrbXLgTI4pYNxuOMmKA0DmlwzHrxMp8b0e4HBI3Pu6q67qDub8xsdaI686BkEAQJAdTp5Qli59TkFxT0M5bT6JZsVDBNult16cdSW8/8BsHPAN8WLxL3Rnm7SGBYQ5EE8b+mBaEB3qukR+00ZH6s6Mw=="
+#define AlipayRSA_PrivateKey @""
 // 商户公钥
-#define AlipayRSA_Public @"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB"
+#define AlipayRSA_Public @""
 /** 极光推送 */
 #define JPushAppkey @"53f6689261b94333775fe561"
 #define JPushTag @"push_service_hospital_"
